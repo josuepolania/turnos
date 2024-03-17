4aa5ca5fdc20875eefbe339e7bcf1d128a0eceab
josue <josuepolania@users.noreply.github.com>
2024-03-16 22:59:24 -0500

    cree paginas, asesorias, turnos y api

--- src/pages/asesoria.jsx
+++ src/pages/asesoria.jsx
@@ -1,11 +1,45 @@
+import React, { useState } from 'react';
+import { ArrowDownIcon } from '@heroicons/react/outline';
+import { llamarTurno } from '../api/turnos';
 
 function Asesoria() {
+  const [showModal, setShowModal] = useState(false);
+  
+  const handleLlamarTurno = () => {
+    llamarTurno()  
+  };
 
-    return (
-      <div>
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-200 border border-black p-4 rounded-lg">
+        <div className="flex justify-center items-center flex-col gap-2">
+          <p className="text-lg font-semibold">Seleccionar Accion</p>
+          <div className="flex items-center justify-center">
+            <ArrowDownIcon className="h-8 w-8 text-gray-500 mt-6 cursor-pointer" />
+          </div>
+          <div className="flex gap-2 py-11">
+            <button className="w-40 h-40 bg-green-500 text-white rounded-lg text-3xl hover:bg-green-600" 
+            onClick={handleLlamarTurno}>Llamar</button>
+            <button className="w-40 h-40 bg-yellow-500 text-white rounded-lg text-3xl hover:bg-yellow-600">No Atendido</button>
+            <button className="w-40 h-40 bg-red-500 text-white rounded-lg text-3xl hover:bg-red-600">Terminado</button>
+          </div>
+        </div>
       </div>
-    )
-  }
-  
-  export default Asesoria
-  
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg">
+            <p>Turno Llamado:</p>
+            <button className="mt-4 bg-blue-500 text-white rounded-lg px-4 py-2" 
+            onClick={handleCloseModal}>Cerrar</button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Asesoria;
--- src/pages/turnos.jsx
+++ src/pages/turnos.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ArrowDownIcon } from '@heroicons/react/outline';
+import { pedirTurno } from '../api/turnos';
 
 function Turnos() {
+  const [showModal, setShowModal] = useState(false);
+  const [turno, setTurno] = useState({});
+
+  // Función para mostrar el modal al darle click al botón 
+  const handleTurnoClick = (tipo) => {
+    const turno = pedirTurno(tipo)
+    setTurno(turno)
+    setShowModal(true);
+  };
+
+  // Función para cerrar el modal al darle click
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
       <div className="w-200 border border-black p-4 rounded-lg">
         <div className="flex justify-center items-center flex-col gap-2">
           <p className="text-lg font-semibold">Seleccionar categoría</p>
           <div className="flex items-center justify-center">
-            <ArrowDownIcon className="h-8 w-8 text-gray-500 mt-6 " />
+            <ArrowDownIcon className="h-8 w-8 text-gray-500 mt-6 cursor-pointer" />
           </div>
           <div className="flex gap-2 py-11">
-            <button className="w-40 h-40 bg-blue-500 text-white rounded-lg text-3xl hover:bg-blue-600">Prioritario</button>
-            <button className="w-40 h-40 bg-green-500 text-white rounded-lg text-3xl hover:bg-green-600">Buena Gente</button>
-            <button className="w-40 h-40 bg-gray-500 text-white rounded-lg text-3xl hover:bg-gray-600">Cliente normal</button>
+            <button className="w-40 h-40 bg-blue-500 text-white rounded-lg text-3xl hover:bg-blue-600"
+              onClick={() => handleTurnoClick("prioritario")}>Prioritario</button>
+            <button className="w-40 h-40 bg-green-500 text-white rounded-lg text-3xl hover:bg-green-600"
+              onClick={() => handleTurnoClick("buenaGente")}>Buena Gente</button>
+            <button className="w-40 h-40 bg-gray-500 text-white rounded-lg text-3xl hover:bg-gray-600"
+              onClick={() => handleTurnoClick("clienteNormal")}>Cliente normal</button>
           </div>
         </div>
       </div>
+      {showModal && (
+        <div className="fixed inset-0 flex items-center justify-center bg-opacity-50">
+          <div className="bg-gray-300 p-8 rounded-lg">
+            <p className='flex text-lg font-bold'>Numero: {turno.numeroTurno} </p>
+            <p>Categoria: {turno.tipo} </p>
+            <p>Fecha: {turno.fechaSolitud.toString()} </p>
+            <div className='flex gap-3'>
+              <button className="mt-4 bg-blue-500 text-white rounded-lg px-4 py-2" onClick={handleCloseModal}>Cerrar</button>
+              <button className="mt-4 bg-blue-500 text-white rounded-lg px-4 py-2" onClick={handleCloseModal}>Imprimir</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default Turnos;
-
